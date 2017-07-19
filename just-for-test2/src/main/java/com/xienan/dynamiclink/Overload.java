f97536007e76c83b0e833176b2ea5255fa175a15
xienan89 <nan.xie89@gmail.com>
2017-07-19 16:25:26 +0800

    tcp test

--- just-for-test2/src/main/java/com/xienan/dynamiclink/Overload.java
+++ just-for-test2/src/main/java/com/xienan/dynamiclink/Overload.java
@@ -7,11 +7,14 @@ package com.xienan.dynamiclink;
 public class Overload {
 	static interface Interman{}
 	static abstract class Human{
+		int i = 0;
 		public static void fun(){
 			System.out.println("human static fun");
 		}
 	}
-	static class Man extends Human implements Interman{}
+	static class Man extends Human implements Interman{
+		int i = 1;
+	}
 	static class Women extends Human{
 		public static void fun(){
 			System.out.println("woman static fun");
@@ -40,6 +43,11 @@ public class Overload {
 		hello(man);
 		Human.fun();
 		women.fun();
+
+        /**
+         * 结果0. man对象其实有两个i变量，用human访问得到父类中的i变量值。在man对象中只能看见子类的i。
+         */
+		System.out.println(human.i);
 	}
 
 }
